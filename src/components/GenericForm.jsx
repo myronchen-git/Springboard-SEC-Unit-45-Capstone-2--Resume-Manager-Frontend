c757bf3d7ed344dc58ef912c14807cb2ab8be901
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-04-16 20:09:38 -0700

    feat: implement form React component
    Created the GenericForm React component, which will manage displaying
    form inputs and will manage user inputs.  This is a more generalized
    component than EducationForm and is meant to allow any section to use
    it for adding or updating.
    
    Note that since GenericForm is going to be used across sections and for
    adding and editing, a unique name needs to be used for the input id.
    This is currently being solved by using the timestamp, which will be
    created every time the form is displayed, or opened.  There should be
    no conflict in the ids between forms, because users would not be able
    to instantaneous open more than two forms at once.
    
    Written tests have been skipped due to time, but basic manual tests
    have been performed.

---
+++ src/components/GenericForm.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { Alert, Button, Form, FormGroup, Input, Label } from 'reactstrap';
+
+import { getFormInputPattern, getFormInputPlaceholder } from '../commonData.js';
+
+// ==================================================
+
+/**
+ * A form to display an education's, experience's, or other's fields, which will
+ * be used to create a new section item or update an existing one.
+ *
+ * @param {Object} props - React component properties.
+ * @param {Object[]} props.fields - Contains a list of Objects with jsName
+ *  (JavaScript name) and displayName to use for populating form inputs.
+ * @param {Number} props.optionalFieldsStartIndex - Index in fields argument
+ *  where the optional fields start.
+ * @param {Object} props.initialFormData - Contains the fields and values to
+ *  show upon opening the form.
+ * @param {Function} props.processSubmission - Handles and submits the form
+ *  data.
+ */
+function GenericForm({
+  fields,
+  optionalFieldsStartIndex,
+  initialFormData,
+  processSubmission,
+}) {
+  const [formData, setFormData] = useState(initialFormData);
+  const [errorMessages, setErrorMessages] = useState([]);
+
+  // --------------------------------------------------
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData((formData) => ({ ...formData, [name]: value }));
+  }
+
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+
+    try {
+      await processSubmission(formData);
+    } catch (err) {
+      setErrorMessages(err);
+      return;
+    }
+  }
+
+  // --------------------------------------------------
+
+  const getRequiredIndicator = (idx) =>
+    idx < optionalFieldsStartIndex ? ' *' : '';
+
+  return (
+    <Form className="GenericForm__form" onSubmit={handleSubmit}>
+      {fields.map((field, idx) => (
+        <FormGroup key={field.jsName} className="text-start">
+          <Label htmlFor={`GenericForm-${Date.now()}__input-${field.jsName}`}>
+            <b>{field.displayName + getRequiredIndicator(idx)}</b>
+          </Label>
+          <Input
+            id={`GenericForm-${Date.now()}__input-${field.jsName}`}
+            type="text"
+            name={field.jsName}
+            placeholder={getFormInputPlaceholder(field.jsName)}
+            pattern={getFormInputPattern(field.jsName)}
+            value={formData[field.jsName]}
+            required={idx < optionalFieldsStartIndex}
+            onChange={handleChange}
+          />
+        </FormGroup>
+      ))}
+      {errorMessages.map((msg) => (
+        <Alert key={msg} color="danger">
+          {msg}
+        </Alert>
+      ))}
+      <Button color="light" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+// ==================================================
+
+export default GenericForm;
