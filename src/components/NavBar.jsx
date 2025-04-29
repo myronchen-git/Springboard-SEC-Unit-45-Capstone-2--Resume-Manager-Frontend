8f84eb689e8cc77ac31914f423c7cf5846a78d1c
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-04-28 19:22:17 -0700

    feat(front-end): add styling for NavBar
    Made the NavBar a sidebar and had it slide open.  Added some simple
    styling for now.  Also renamed collapsed to isOpen to follow JavaScript
    naming conventions.
    
    Some of the styles had to be set through the style prop for Offcanvas
    and Button, else it will be overridden.  Bootstrap shadows were used by
    assigning the classes to elements, because they looked better than
    manually created ones.
    
    Code has been adapted from ChatGPT.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,41 +1,78 @@
 import { useContext, useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import { Collapse, Nav, Navbar, NavbarToggler, NavItem } from 'reactstrap';
+import {
+  Button,
+  Nav,
+  Navbar,
+  NavItem,
+  Offcanvas,
+  OffcanvasBody,
+  OffcanvasHeader,
+} from 'reactstrap';
 
 import { UserContext } from '../contexts.jsx';
+
+import './NavBar.css';
 
 // ==================================================
 
 function NavBar() {
-  const [collapsed, setCollapsed] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const { signoutUser } = useContext(UserContext);
 
   // --------------------------------------------------
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
+  const toggleNavbar = () => setIsOpen(!isOpen);
 
   // --------------------------------------------------
 
   return (
     <>
-      <Navbar className="NavBar">
-        <NavbarToggler onClick={toggleNavbar} />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink to="/document">Document</NavLink>
+      <Navbar className="NavBar" fixed="top" light>
+        {!isOpen && (
+          <Button
+            className="NavBar__menu-button shadow-sm"
+            color="light"
+            onClick={toggleNavbar}
+            style={{ backgroundColor: 'lightblue' }}
+          >
+            ☰ Menu
+          </Button>
+        )}
+      </Navbar>
+
+      <Offcanvas
+        className="NavBar__menu shadow"
+        isOpen={isOpen}
+        toggle={toggleNavbar}
+        direction="start"
+        backdrop={false}
+        tag="aside"
+        style={{ width: '15em', backgroundColor: '#add8e680', border: 'none' }}
+      >
+        <OffcanvasHeader toggle={toggleNavbar} wrapTag="header">
+          Resume Manager
+        </OffcanvasHeader>
+        <OffcanvasBody tag="nav">
+          <Nav vertical>
+            <NavItem className="NavBar__menu__item">
+              <NavLink to="/document" onClick={toggleNavbar}>
+                Document
+              </NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink to="/account">Account</NavLink>
+            <NavItem className="NavBar__menu__item">
+              <NavLink to="/account" onClick={toggleNavbar}>
+                Account
+              </NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem className="NavBar__menu__item">
               <NavLink to="/" onClick={signoutUser}>
                 Sign Out
               </NavLink>
             </NavItem>
           </Nav>
-        </Collapse>
-      </Navbar>
+        </OffcanvasBody>
+      </Offcanvas>
       <Outlet />
     </>
   );
