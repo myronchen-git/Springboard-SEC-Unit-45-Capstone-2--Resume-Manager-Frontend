4a98ff35b1c89108823249d5ca3f06d6b5ddbd7f
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-05-01 19:27:41 -0700

    feat(front-end)!: stylize homepage
    Added a background image.  The image is by BiljaST and from pixabay
    (https://pixabay.com/photos/resume-cv-job-application-career-6627200/).
    
    Added a Google font, Libre Baskerville, through embedment in the <html>
    <head>.
    
    Set the "body" element to have at minimum 100vh and 100vw, so that any
    children can take up the whole viewing area in the browser.
    Concurringly, applied 100% height and width and removed padding for the
    "#root" CSS selector.
    
    BREAKING CHANGE: Since padding is removed, other webpages are now
    broken.

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
 
+import './HomePage.css';
+
 // ==================================================
 
 /**
@@ -9,18 +11,20 @@ import { Button } from 'reactstrap';
 function HomePage() {
   return (
     <main className="HomePage">
-      <h1>Resume Manager</h1>
-      <p>
-        Store all of your information in one resume and create tailored resumes
-        and templates from it that automatically update themselves.
-      </p>
-      <div>
-        <Link to="/register">
-          <Button color="primary">Register</Button>
-        </Link>
-        <Link to="/signin">
-          <Button color="primary">Sign In</Button>
-        </Link>
+      <div className="HomePage-backdrop">
+        <h1 className="HomePage-title">Resume Manager</h1>
+        <p className="HomePage-description">
+          Store all of your information in one resume and create tailored
+          resumes from it that automatically update themselves.
+        </p>
+        <div className="HomePage-buttons">
+          <Link to="/register">
+            <Button color="primary">Register</Button>
+          </Link>
+          <Link to="/signin">
+            <Button color="primary">Sign In</Button>
+          </Link>
+        </div>
       </div>
     </main>
   );
